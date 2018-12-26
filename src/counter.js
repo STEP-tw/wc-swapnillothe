c888b5d074094b33d205b700e7aaed379e0c9b50
swapnil lothe <44019530+swapnillothe@users.noreply.github.com>
2018-12-26 14:19:56 +0530

    Introduce a new features of counting words from file content.
    ugly way of parsing args in the same function.

--- src/counter.js
+++ src/counter.js
@@ -2,13 +2,21 @@ const countLines = function (text) {
   return text.split('\n').length;
 }
 
-const countChars = (text)=> text.length;
+const countChars = (text) => text.length;
+
+const countWords = function (text) {
+  let textWithoutNewLine = text.replace(/\n/g, ' ');
+  return textWithoutNewLine.split(' ').length;
+}
 
 const wc = function (args, fs) {
-  let file = fs.readFileSync(args[1],'utf8');
-  let count = countLines(file)-1;
-  if(args[0]=='-c'){
+  let file = fs.readFileSync(args[1], 'utf8');
+  let count = countLines(file) - 1;
+  if (args[0] == '-c') {
     count = countChars(file);
+  }
+  if (args[0] == '-w') {
+    count = countWords(file);
   }
   return `${count} ${args[1]}`
 }
--- test/counterTest.js
+++ test/counterTest.js
@@ -9,20 +9,27 @@ describe('wc', function () {
       readFileSync: (fileName) => file[fileName],
       existSync: (fileName) => file.hasOwnProperty(fileName)
     }
-    const file = { 'numbers.txt': '1\n2\n3\n4\n5\n6\n7\n8\n8\n10\n' };
+    const file = { 'numbers.txt': '1\n2\n3\n4\n5\n6\n7\n8\n8\n10' };
   })
 
   it('should return count of line and file name', function () {
     let args = ['-l', 'numbers.txt'];
     let actualOutput = wc(args, fs);
-    let expectedOutput = '10 numbers.txt';
+    let expectedOutput = '9 numbers.txt';
     assert.deepEqual(actualOutput, expectedOutput);
   });
 
-  it('should return count of characters and file name', function () {
+  it('should return count of characters and file name when option -c is specified', function () {
     let args = ['-c', 'numbers.txt'];
     let actualOutput = wc(args, fs);
-    let expectedOutput = '21 numbers.txt';
+    let expectedOutput = '20 numbers.txt';
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+
+  it('should return count of word and file name when option -w is speci', function () {
+    let args = ['-w', 'numbers.txt'];
+    let actualOutput = wc(args, fs);
+    let expectedOutput = '10 numbers.txt';
     assert.deepEqual(actualOutput, expectedOutput);
   });
 });
